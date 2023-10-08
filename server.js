553d779f1930a05b242b8a7dde2f36bc30af498a
johnmiicheal <michealelijah301@gmail.com>
2023-10-08 10:34:14 +0100

    feat: migrated to supabase

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const axios = require("axios"); // You may need to install axios if not already installed
 const { Configuration, OpenAIApi } = require("openai");
-const { PineconeClient } = require("@pinecone-database/pinecone");
-const { setCookie } = require("./utils/cookies");
+const { createClient } = require("@supabase/supabase-js");
 const cors = require("cors");
 require("dotenv").config()
 
 const app = express();
-const port = 4000;
 
 app.use(cors());
 
@@ -17,8 +16,8 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 if (!configuration.apiKey) {
-  console.log("Error");
-  // return;
+  console.log("Error: OpenAI API key not properly configured");
+  process.exit(1); // Terminate the Node.js process
 } else {
   console.log("It's working");
 }
@@ -26,46 +25,44 @@ if (!configuration.apiKey) {
 // Declare constants
 const COMPLETIONS_MODEL = "text-davinci-003";
 const EMBEDDING_MODEL = "text-embedding-ada-002";
+const supaUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supaKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supaUrl, supaKey);
 
-app.use(express.json());
+app.use(express.json()); // Parse JSON requests
 
 app.post("/api/api", async (req, res) => {
-  console.log(req.body);
-  const { token } = req.body;
-  setCookie(res, "token", token, { path: "https://lecturemate.vercel.app/app/chat", maxAge: 2592000 });
-  const setTokenKey = res.getHeader("Set-Cookie")[0];
-  console.log("Log Token", token);
-  const regex = /token=([^;]+)/;
-  const match = setTokenKey.match(regex);
-  const tokenValue = token;
-  console.log(`lecture-mate-${tokenValue}`);
+  const json = req.body;
+  const nameOfFile = localStorage.getItem("file"); // Replace with your logic to get the file name
+
+  const userId = json.userId;
+  console.log("This is the json: ", nameOfFile);
+
+  const { data: pdfData } = await supabase
+    .from("pdfs")
+    .select("*")
+    .eq("pdf_name", nameOfFile)
+    .eq("user_id", userId);
+
+  console.log("extracted: ", JSON.stringify(pdfData));
+
+  if (pdfData !== null) {
+    console.log("length of file: ", pdfData.length);
+  }
 
   if (!configuration.apiKey) {
-    res.status(500).json({
-      error: {
-        message: "OpenAI API key not properly configured",
-      },
-    });
+    console.error("OpenAI API key not properly configured");
+    res.status(500).json({ error: "OpenAI API key not properly configured" });
     return;
   }
 
-  const query = req.body.query || "";
+  const query = json.query || "";
 
   if (query.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a question",
-      },
-    });
+    console.error("Please enter a question");
+    res.status(500).json({ error: "Please enter a question" });
     return;
   }
-
-  const pinecone = new PineconeClient();
-  await pinecone.init({
-    environment: "us-central1-gcp",
-    apiKey: "05cb4d94-686d-4d1b-b412-fb561175026b",
-  });
-  console.log("pinecone point:", query);
 
   const queryEmbedding = await openai.createEmbedding({
     model: EMBEDDING_MODEL,
@@ -75,54 +72,93 @@ app.post("/api/api", async (req, res) => {
 
   const xq = queryEmbedding.data.data[0].embedding;
 
-  const queryIndex = pinecone.Index("lecture-mate");
+  console.log("embedding: " + xq);
 
-  const queryRes = await queryIndex.query({
-    queryRequest: {
-      namespace: `lecture-mate-${tokenValue}`,
-      vector: xq,
-      topK: 1,
-      includeMetadata: true,
-    },
-  });
-  let info = "";
-  if (queryRes && queryRes.matches && queryRes.matches.length > 0) {
-    const firstMatch = queryRes.matches[0];
-    if (firstMatch.metadata && typeof firstMatch.metadata.text === "string") {
-      info = firstMatch.metadata.text;
+  function calculateDotProductSimilarity(vector1, vector2) {
+    if (vector1.length !== vector2.length) {
+      throw new Error("Vector dimensions do not match");
     }
+
+    let dotProduct = 0;
+    for (let i = 0; i < vector1.length; i++) {
+      dotProduct += vector1[i] * vector2[i];
+    }
+
+    console.log("It's me Ifiok2");
+
+    return dotProduct;
   }
-  console.log("Query Info:", info);
-  const finalPrompt = `
-      Info: ${info}
-      Question: ${query}. 
-      Answer:
-    `;
-  try {
-    const response = await openai.createCompletion({
-      model: COMPLETIONS_MODEL,
-      prompt: finalPrompt,
-      max_tokens: 2048,
+
+  async function calculateSimilarityScores(userQueryEmbedding, pdfData) {
+    const similarityScores = [];
+
+    pdfData.forEach((row) => {
+      const pageEmbedding = row.vector_data;
+      const similarity = calculateDotProductSimilarity(userQueryEmbedding, pageEmbedding);
+
+      similarityScores.push({
+        pageData: row,
+        similarity: similarity,
+      });
     });
 
-    const completion = response.data.choices[0].text;
-    console.log(completion);
-    res.status(200).send({ query, result: completion });
-  } catch (error) {
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: "An error occurred during your request.",
-        },
+    // Sort by similarity in descending order
+    similarityScores.sort((a, b) => b.similarity - a.similarity);
+
+    // Select the top 5 pages
+    const top5SimilarPages = similarityScores.slice(0, 5);
+    console.log(top5SimilarPages);
+
+    // To get the results
+
+    const mostSimilar = top5SimilarPages[0].pageData.page_text;
+    const inputText = mostSimilar;
+
+    const plainText = inputText.replace(/[+\n]/g, '');
+
+    console.log(plainText);
+
+    console.log("Query Info:", plainText);
+    const finalPrompt = `
+        Info: Using this info: ${plainText} make the answer as explanatory as possible. With points and examples
+        Question: ${query}.
+        Answer:
+      `;
+
+    try {
+      const response = await openai.createCompletion({
+        model: COMPLETIONS_MODEL,
+        prompt: finalPrompt,
+        max_tokens: 2048,
       });
+
+      const completion = response.data.choices[0].text;
+      console.log(completion);
+      console.log(query);
+
+      const result = {
+        query: query,
+        completion: completion,
+      };
+
+      console.log("Funny how this will work: " + JSON.stringify(result));
+
+      res.status(200).json(result);
+    } catch (error) {
+      if (error.response) {
+        console.error(error.response.status, error.response.data);
+        res.status(error.response.status).json(error.response.data);
+      } else {
+        console.error(`Error with request: ${error.message}`);
+        res.status(500).json({ error: "An error occurred during your request." });
+      }
     }
   }
+
+  await calculateSimilarityScores(xq, pdfData);
 });
 
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
