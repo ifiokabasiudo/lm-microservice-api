853e21bdc61a800f028a105dfccfa316d2d0ea6f
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-10-15 18:29:43 +0100

    Added error handling

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ const supabase = createClient(supaUrl, supaKey);
 app.use(express.json()); // Parse JSON requests
 
 app.post("/api/api", async (req, res) => {
-  const json = req.body;
+  try {
+    const json = req.body;
   const nameOfFile = json.nameOfFile; // Replace with your logic to get the file name
   const userId = json.userId;
   console.log("This is the json: ", nameOfFile);
@@ -77,310 +78,369 @@ app.post("/api/api", async (req, res) => {
   console.log("embedding: " + xq);
 
   const createUser = async (finalPrompt) => {
-    const { data, error } = await supabase
-    .from('chats')
-    .insert([{ user_id: userId,  chats: [{role: 'user', content: finalPrompt}]}])
-    .select()
-
-    console.log(data)
+    try{
+      const { data, error } = await supabase
+      .from('chats')
+      .insert([{ user_id: userId,  chats: [{role: 'user', content: finalPrompt}]}])
+      .select()
+  
+      console.log(data)
+    }catch(err){
+      console.log(err)
+    }
   }
 
   const upsertAssistant = async (response) => {
-    const { data: rowData, error } = await supabase
-    .from('chats')
-    .select('chats')
-    .eq('user_id', userId);
-
-    if (rowData && rowData.length > 0) {
-      const currentArray = rowData[0].chats;
-      const newValue = {role: 'assistant', content: response};
-      
-      const updatedArray = [...currentArray, newValue];           
-      // You can also perform other modifications as needed.
-
-      if (updatedArray) {
-        const { data: updatedData, error: updateError } = await supabase
-          .from('chats')
-          .update({ chats: updatedArray })
-          .eq('user_id', userId);
-      
-        if (updateError) {
-          // Handle the update error.
-          console.log(updateError)
-        } else {
-          // Handle the successful update.
-          console.log(updatedData)
+    try{
+      const { data: rowData, error } = await supabase
+      .from('chats')
+      .select('chats')
+      .eq('user_id', userId);
+  
+      if (rowData && rowData.length > 0) {
+        const currentArray = rowData[0].chats;
+        const newValue = {role: 'assistant', content: response};
+        
+        const updatedArray = [...currentArray, newValue];           
+        // You can also perform other modifications as needed.
+  
+        if (updatedArray) {
+          try {
+            const { data: updatedData, error: updateError } = await supabase
+            .from('chats')
+            .update({ chats: updatedArray })
+            .eq('user_id', userId);
+        
+          if (updateError) {
+            // Handle the update error.
+            console.log(updateError)
+          } else {
+            // Handle the successful update.
+            console.log(updatedData)
+          }
+          } catch (err) {
+            console.log(err)
+          }
         }
       }
+    }catch(err) {
+      console.log(err)
     }
   }
 
   const upsertUser = async (finalPrompt) => {
-    const { data: rowData, error } = await supabase
-    .from('chats')
-    .select('chats')
-    .eq('user_id', userId);
-
-    if (rowData && rowData.length > 0) {
-      const currentArray = rowData[0].chats;
-      const newValue = {role: 'user', content: finalPrompt};
-      
-      const updatedArray = [...currentArray, newValue];           
-      // You can also perform other modifications as needed.
-
-      if (updatedArray) {
-        const { data: updatedData, error: updateError } = await supabase
-          .from('chats')
-          .update({ chats: updatedArray })
-          .eq('user_id', userId);
-      
-        if (updateError) {
-          // Handle the update error.
-          console.log(updateError)
-        } else {
-          // Handle the successful update.
-          console.log(updatedData)
+    try{
+      const { data: rowData, error } = await supabase
+      .from('chats')
+      .select('chats')
+      .eq('user_id', userId);
+  
+      if (rowData && rowData.length > 0) {
+        const currentArray = rowData[0].chats;
+        const newValue = {role: 'user', content: finalPrompt};
+        
+        const updatedArray = [...currentArray, newValue];           
+        // You can also perform other modifications as needed.
+  
+        if (updatedArray) {
+          try {
+            const { data: updatedData, error: updateError } = await supabase
+            .from('chats')
+            .update({ chats: updatedArray })
+            .eq('user_id', userId);
+        
+          if (updateError) {
+            // Handle the update error.
+            console.log(updateError)
+          } else {
+            // Handle the successful update.
+            console.log(updatedData)
+          }
+          } catch (err) {
+            console.log(err)
+          }
         }
       }
-    }
+    }catch(err){
+      console.log(err)
+    }    
   }
 
   const checkIfRowExists = async (finalPrompt) => {
-    const condition = { column_value: userId }; // Replace with your own condition
+    try{
+      const condition = { column_value: userId }; // Replace with your own condition
 
-    const { data, error } = await supabase
-    .from('chats')
-    .select()
-    .eq('user_id', condition.column_value);
-
-    if (data && data.length > 0) {
-      upsertUser(finalPrompt)
-    } else {
-      createUser(finalPrompt)
+      const { data, error } = await supabase
+      .from('chats')
+      .select()
+      .eq('user_id', condition.column_value);
+  
+      if (data && data.length > 0) {
+        upsertUser(finalPrompt)
+      } else {
+        createUser(finalPrompt)
+      }
+    }catch(err){
+      console.log(err)
     }
+    
   }
 
   const getChatHistory = async () => {
-    const condition = { column_value: userId }; // Replace with your own condition
+    try{
+      const condition = { column_value: userId }; // Replace with your own condition
 
-    function delay(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
-    const data = await delay(5000).then(async () => {
-      const { data, error } = await supabase
-        .from('chats')
-        .select()
-        .eq('user_id', condition.column_value);
-  
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("This is the get chat history: " + JSON.stringify(data[0].chats));
-        return data[0].chats;
+      function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
       }
-    });
   
-    return data;
+      const data = await delay(5000).then(async () => {
+        try {
+          const { data, error } = await supabase
+          .from('chats')
+          .select()
+          .eq('user_id', condition.column_value);
+    
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("This is the get chat history: " + JSON.stringify(data[0].chats));
+          return data[0].chats;
+        }
+        } catch (err) {
+         console.log(err) 
+        }
+      });
+    
+      return data;
+    }catch(err){
+      console.log(err)
+    }    
   }
 
   const deleteLastQuestion = async () => {
-    const { data: rowData, error } = await supabase
-    .from('chats')
-    .select('chats')
-    .eq('user_id', userId);
-
-    if (rowData && rowData.length > 0) {
-      const newArray = rowData[0].chats;
-      const revertedArray = newArray.pop();
-
-      if (revertedArray) {
-        const { data: revertedData, error: revertError } = await supabase
-          .from('chats')
-          .update({ chats: revertedArray })
-          .eq('user_id', userId);
-      
-        if (revertError) {
-          // Handle the update error.
-          console.log(revertError)
-        } else {
-          // Handle the successful update.
-          console.log(revertedData)
+    try{
+      const { data: rowData, error } = await supabase
+      .from('chats')
+      .select('chats')
+      .eq('user_id', userId);
+  
+      if (rowData && rowData.length > 0) {
+        const newArray = rowData[0].chats;
+        const revertedArray = newArray.pop();
+  
+        if (revertedArray) {
+          try {
+            const { data: revertedData, error: revertError } = await supabase
+            .from('chats')
+            .update({ chats: revertedArray })
+            .eq('user_id', userId);
+        
+          if (revertError) {
+            // Handle the update error.
+            console.log(revertError)
+          } else {
+            // Handle the successful update.
+            console.log(revertedData)
+          }          
+          } catch (err) {
+            console.log(err)
+          }
         }
       }
-    }
+    }catch(err){
+      console.log(err)
+    }    
   }
 
   const processAnswers = async () => {
-
-    function delay(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
-      const result = delay(7000).then(async () => {
-      const history = await getChatHistory()
-      console.log("This is the history: " + history)
-      console.log("This is the history: " + JSON.stringify(history))
-
-      
-        const chatCompletion = await openai.chat.completions.create({
-          messages: history,
-          model: 'gpt-3.5-turbo',
-          max_tokens: 2048,
-        });
-      
-        console.log(chatCompletion.choices);
-        
-        const chatResponse = chatCompletion.choices[0].message.content
-
-        if(!chatResponse){
-          deleteLastQuestion()
-        }else{
-          upsertAssistant(chatResponse)
+    try {
+      function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+  
+        const result = delay(7000).then(async () => {
+        try {
+          const history = await getChatHistory()
+          console.log("This is the history: " + history)
+          console.log("This is the history: " + JSON.stringify(history))
+    
+          
+            const chatCompletion = await openai.chat.completions.create({
+              messages: history,
+              model: 'gpt-3.5-turbo',
+              max_tokens: 2048,
+            });
+          
+            console.log(chatCompletion.choices);
+            
+            const chatResponse = chatCompletion.choices[0].message.content
+    
+            if(!chatResponse || chatResponse === null){
+              deleteLastQuestion()
+            }else{
+              upsertAssistant(chatResponse)
+            }
+            
+            const history2 = await getChatHistory()
+    
+            const result= {
+              query: history2,
+              completion: chatResponse
+            }
+    
+            return result  
+        } catch (err) {
+          console.log(err)
         }
-        
-        const history2 = await getChatHistory()
-
-        const result= {
-          query: history2,
-          completion: chatResponse
-        }
-
-        return result 
-    })
-    return result
+      })
+      return result
+    } catch (err) {
+      console.log(err)
+    }    
   }
 
   function calculateDotProductSimilarity(vector1, vector2) {
-    if (vector1.length !== vector2.length) {
-      throw new Error("Vector dimensions do not match");
+    try {
+      if (vector1.length !== vector2.length) {
+        throw new Error("Vector dimensions do not match");
+      }
+  
+      let dotProduct = 0;
+      for (let i = 0; i < vector1.length; i++) {
+        dotProduct += vector1[i] * vector2[i];
+      }
+      return dotProduct;
+    } catch (err) {
+      console.log(err)
     }
-
-    let dotProduct = 0;
-    for (let i = 0; i < vector1.length; i++) {
-      dotProduct += vector1[i] * vector2[i];
-    }
-    return dotProduct;
   }
 
   async function calculateSimilarityScores(userQueryEmbedding, pdfData) {
-    const similarityScores = [];
-    pdfData.forEach((row) => {
-      const pageEmbedding = row.vector_data;
-      const similarity = calculateDotProductSimilarity(
-        userQueryEmbedding,
-        pageEmbedding
-      );
-
-      similarityScores.push({
-        pageData: row,
-        similarity: similarity,
+    try {
+      const similarityScores = [];
+      pdfData.forEach((row) => {
+        const pageEmbedding = row.vector_data;
+        const similarity = calculateDotProductSimilarity(
+          userQueryEmbedding,
+          pageEmbedding
+        );
+  
+        similarityScores.push({
+          pageData: row,
+          similarity: similarity,
+        });
       });
-    });
-
-    // Sort by similarity in descending order
-    similarityScores.sort((a, b) => b.similarity - a.similarity);
-
-    if (similarityScores.length > 0) {
-      // Select the top 5 pages
-      const top5SimilarPages = similarityScores.slice(0, 5);
-      console.log(top5SimilarPages);
-
-      // To get the results
-
-      const mostSimilar = top5SimilarPages[0].pageData.page_text;
-      const inputText = mostSimilar;
-
-      const plainText = inputText.replace(/[+\n]/g, "");
-
-      console.log(plainText);
-
-      console.log("Query Info:", plainText);
-      const finalPrompt = `
-            Info: Using this info: ${plainText} make the answer as explanatory as possible. With points and examples
-            Question: ${query}.
-            Answer:
-          `;
-
-      try {
-
-        checkIfRowExists(finalPrompt)
-        const result = await processAnswers()
-
-        // const response = await openai.createCompletion({
-        //   model: COMPLETIONS_MODEL,
-        //   prompt: finalPrompt,
-        //   max_tokens: 2048,
-        // });
-
-        // const completion = response.data.choices[0].text;
-        // console.log(completion);
-        // console.log(query);
-
-        // const result = {
-        //   query: query,
-        //   completion: completion,
-        // };
-
-        console.log("Funny how this will work: " + JSON.stringify(result));
-
-        res.status(200).json(result);
-      } catch (error) {
-        if (error.response) {
-          console.error(error.response.status, error.response.data);
-          res.status(error.response.status).json(error.response.data);
-        } else {
-          console.error(`Error with request: ${error.message}`);
-          res
-            .status(500)
-            .json({ error: "An error occurred during your request." });
+  
+      // Sort by similarity in descending order
+      similarityScores.sort((a, b) => b.similarity - a.similarity);
+  
+      if (similarityScores.length > 0) {
+        // Select the top 5 pages
+        const top5SimilarPages = similarityScores.slice(0, 5);
+        console.log(top5SimilarPages);
+  
+        // To get the results
+  
+        const mostSimilar = top5SimilarPages[0].pageData.page_text;
+        const inputText = mostSimilar;
+  
+        const plainText = inputText.replace(/[+\n]/g, "");
+  
+        console.log(plainText);
+  
+        console.log("Query Info:", plainText);
+        const finalPrompt = `
+              Info: Using this info: ${plainText} make the answer as explanatory as possible. With points and examples
+              Question: ${query}.
+              Answer:
+            `;
+  
+        try {
+  
+          checkIfRowExists(finalPrompt)
+          const result = await processAnswers()
+  
+          // const response = await openai.createCompletion({
+          //   model: COMPLETIONS_MODEL,
+          //   prompt: finalPrompt,
+          //   max_tokens: 2048,
+          // });
+  
+          // const completion = response.data.choices[0].text;
+          // console.log(completion);
+          // console.log(query);
+  
+          // const result = {
+          //   query: query,
+          //   completion: completion,
+          // };
+  
+          console.log("Funny how this will work: " + JSON.stringify(result));
+  
+          res.status(200).json(result);
+        } catch (error) {
+          if (error.response) {
+            console.error(error.response.status, error.response.data);
+            res.status(error.response.status).json(error.response.data);
+          } else {
+            console.error(`Error with request: ${error.message}`);
+            res
+              .status(500)
+              .json({ error: "An error occurred during your request." });
+          }
+        }
+      } else {
+        // Handle the case where there are no similarity scores
+        console.log("No similarity scores found.");
+      //   const finalPrompt = `
+      //   Info: Welcome the user to Lecture Mate in a polite manner and ask how you can be of service. You can use different approaches to welcome the user but always be friendly.
+      //   Question: ${query}.
+      //   Answer:
+      // `;
+  
+        try {
+          checkIfRowExists(query)
+          const result = await processAnswers()
+          // const response = await openai.createCompletion({
+          //   model: COMPLETIONS_MODEL,
+          //   prompt: finalPrompt,
+          //   max_tokens: 2048,
+          // });
+  
+          // const completion = response.data.choices[0].text;
+          // console.log(completion);
+          // console.log(query);
+  
+          // const result = {
+          //   query: query,
+          //   completion: completion,
+          // };
+  
+          // console.log("Funny how this will work: " + JSON.stringify(result));
+  
+          res.status(200).json(result);
+        } catch (error) {
+          if (error.response) {
+            console.error(error.response.status, error.response.data);
+            res.status(error.response.status).json(error.response.data);
+          } else {
+            console.error(`Error with request: ${error.message}`);
+            res
+              .status(500)
+              .json({ error: "An error occurred during your request." });
+          }
         }
       }
-    } else {
-      // Handle the case where there are no similarity scores
-      console.log("No similarity scores found.");
-    //   const finalPrompt = `
-    //   Info: Welcome the user to Lecture Mate in a polite manner and ask how you can be of service. You can use different approaches to welcome the user but always be friendly.
-    //   Question: ${query}.
-    //   Answer:
-    // `;
-
-      try {
-        checkIfRowExists(query)
-        const result = await processAnswers()
-        // const response = await openai.createCompletion({
-        //   model: COMPLETIONS_MODEL,
-        //   prompt: finalPrompt,
-        //   max_tokens: 2048,
-        // });
-
-        // const completion = response.data.choices[0].text;
-        // console.log(completion);
-        // console.log(query);
-
-        // const result = {
-        //   query: query,
-        //   completion: completion,
-        // };
-
-        // console.log("Funny how this will work: " + JSON.stringify(result));
-
-        res.status(200).json(result);
-      } catch (error) {
-        if (error.response) {
-          console.error(error.response.status, error.response.data);
-          res.status(error.response.status).json(error.response.data);
-        } else {
-          console.error(`Error with request: ${error.message}`);
-          res
-            .status(500)
-            .json({ error: "An error occurred during your request." });
-        }
-      }
-    }
+    } catch (err) {
+      console.log(err)
+    }    
   }
 
   await calculateSimilarityScores(xq, pdfData);
+  } catch (err) {
+    console.log(err)
+  }
 });
 
 const port = 4000;
